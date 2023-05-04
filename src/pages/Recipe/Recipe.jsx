c7253f248dd65105bf21fb0eb261ef65faa8eb51
ND Morsalin <morsalin056@gmail.com>
2023-05-04 08:51:18 -0700

    when click on Add to favorite button a tost showing added

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,8 +1,9 @@
-import { Link, useLoaderData, useNavigation } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import Banner from "../../components/Recipe/Banner/Banner";
 import Loading from "../../Shared/Loading/Loading";
-import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
+import { toast } from "react-hot-toast";
+import RecipeCard from "./RecipeCard";
 
 const Star = (
   <path d="M62 25.154H39.082L32 3l-7.082 22.154H2l18.541 13.693L13.459 61L32 47.309L50.541 61l-7.082-22.152L62 25.154z" />
@@ -27,7 +28,12 @@ const Recipe = () => {
   if (navigation.state === "loading") {
     return <Loading />;
   }
-  console.log(data.recipes);
+
+  const handleAddToFavorite = async (recipe_id) => {
+    toast("Added to favorite")
+    console.log("add to favorite",recipe_id);
+  };
+
   return (
     <div className="container mx-auto py-6 sm:py-8 sm:px-8 rounded-lg space-y-8">
       <Banner chef={data} />
@@ -35,66 +41,7 @@ const Recipe = () => {
         {
           // ! change color
           data.recipes.map((recipe) => (
-            <div key={recipe.recipe_id} className="">
-              <div className="grid grid-cols-6 gap-2 bg-green-100 rounded-lg">
-                <div className="card  shadow-xl text-black col-span-3 p-4">
-                  <figure className="px-2 pt-4 mx-auto  rounded-full overflow-hidden ">
-                    <img
-                      src={recipe?.recipe_img}
-                      alt="Shoes"
-                      className="w-24 h-24 rounded-full object-cover"
-                    />
-                  </figure>
-                  <div className="card-body items-center text-center p-2 py-4">
-                    <h2 className="card-title text-2xl mt-0">
-                      {recipe.recipe_name}
-                    </h2>
-                    <div className="">
-                      <h2 className="card-title text-xl mt-0"> ingredients</h2>
-
-                      <div className="flex gap-3 items-center flex-wrap">
-                        {recipe.ingredients.map((ingredient) => (
-                          <div
-                            key={ingredient}
-                            className="flex items-center gap-2"
-                          >
-                            <div className="badge badge-accent">
-                              {ingredient}
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                    <div className="flex items-center justify-center gap-2 flex-wrap">
-                      <Rating
-                        style={{ maxWidth: 400 }}
-                        value={recipe.rating}
-                        itemStyles={myStyles}
-                        readOnly
-                        radius="large"
-                        className="gap-2"
-                        halfFillMode="box"
-                      />
-                      <div className="w-8 mx-auto h-8 flex items-center justify-center font-bold  border-2 border-base-200 rounded-full">
-                        {recipe.rating}
-                      </div>
-                    </div>
-                    <div className="card-actions w-full">
-                      <Link
-                to={`/recipe/${recipe.recipe_id}`}
-                className="btn btn-info w-full"
-              >
-                Add to Favorite
-              </Link>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-span-3 p-4">
-                  <h4 className="text-lg font-bold">Cooking Method</h4>
-                  <p className="">{recipe.cooking_method}</p>
-                </div>
-              </div>
-            </div>
+            <RecipeCard key={recipe.recipe_id} recipe={recipe} />
           ))
         }
       </div>
---
+++ src/pages/Recipe/RecipeCard.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/prop-types */
+import "@smastrom/react-rating/style.css";
+import { toast } from "react-hot-toast";
+import {Rating} from '@smastrom/react-rating'
+import { useState } from "react";
+
+const Star = (
+  <path d="M62 25.154H39.082L32 3l-7.082 22.154H2l18.541 13.693L13.459 61L32 47.309L50.541 61l-7.082-22.152L62 25.154z" />
+);
+const myStyles = {
+  itemShapes: Star,
+  boxBorderWidth: 1,
+
+  activeFillColor: ["#FEE2E2", "#FFEDD5", "#FEF9C3", "#ECFCCB", "#D1FAE5"],
+  activeBoxColor: ["#da1600", "#db711a", "#dcb000", "#61bb00", "#009664"],
+  activeBoxBorderColor: ["#c41400", "#d05e00", "#cca300", "#498d00", "#00724c"],
+
+  inactiveFillColor: "white",
+  inactiveBoxColor: "#dddddd",
+  inactiveBoxBorderColor: "#a8a8a8",
+};
+
+const RecipeCard = ({ recipe }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+  const handleAddToFavorite = async (recipe_id) => {
+    toast("Added to favorite");
+    setIsClicked(true);
+    console.log("add to favorite", recipe_id);
+
+  };
+
+
+
+  return (
+    <div className="">
+      <div className="flex flex-wrap  bg-green-100 rounded-lg">
+        <div className="card  shadow-xl text-black sm:flex-1 p-4">
+          <figure className="px-2 pt-4 mx-auto  rounded-full overflow-hidden ">
+            <img
+              src={recipe?.recipe_img}
+              alt="Shoes"
+              className="w-24 h-24 rounded-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center p-2 py-4">
+            <h2 className="card-title text-2xl mt-0">{recipe.recipe_name}</h2>
+            <div className="">
+              <h2 className="card-title text-xl mt-0"> ingredients</h2>
+
+              <div className="flex gap-3 items-center flex-wrap">
+                {recipe.ingredients.map((ingredient) => (
+                  <div key={ingredient} className="flex items-center gap-2">
+                    <div className="badge badge-primary ">{ingredient}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex items-center justify-center gap-2 flex-wrap">
+              <Rating
+                style={{ maxWidth: 400 }}
+                value={recipe.rating}
+                itemStyles={myStyles}
+                readOnly
+                radius="large"
+                className="gap-2"
+                halfFillMode="box"
+              />
+              <div className="w-8 mx-auto h-8 flex items-center justify-center font-bold  border-2 border-base-200 rounded-full">
+                {recipe.rating}
+              </div>
+            </div>
+            <div className="card-actions w-full">
+              <button
+                onClick={() => {
+                  handleAddToFavorite(recipe.recipe_id);
+                }}
+                disabled={isClicked}
+                className={`btn  w-full ${isClicked? "btn-secondary" : "btn-info"}`}
+              >
+                Add to Favorite
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="sm:flex-1 p-4">
+          <h4 className="text-lg font-bold">Cooking Method</h4>
+          <p className="">{recipe.cooking_method}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
